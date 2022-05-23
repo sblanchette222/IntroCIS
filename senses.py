35c7a247e7080a0329f86a5baf3a81a7cb331d8c
sblanchette222 <67975226+sblanchette222@users.noreply.github.com>
2022-05-23 06:53:54 -0700

    Update senses.py

--- senses.py
+++ senses.py
@@ -1,1 +1,20 @@
 
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+#declare a temperature variable and assign it the value of sense.get_temperature()
+temp = sense.get_temperature()
+
+#print out type statements:
+
+print("The temperature is", temp)
+sense.show_message(temp)
+
+
+#make a while True loop
+
+
+
+    #add conditional statements to test
