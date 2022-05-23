4e1d70363aef0d91c191f6cb178856eb6b97d9e2
sblanchette222 <67975226+sblanchette222@users.noreply.github.com>
2022-05-23 07:00:39 -0700

    Update senses.py

--- senses.py
+++ senses.py
@@ -6,10 +6,17 @@ sense = SenseHat()
 
 #declare a temperature variable and assign it the value of sense.get_temperature()
 temp = sense.get_temperature()
+pressure = sense.get_pressure()
 
 #print out type statements:
 
 print("The temperature is", temp)
+print("The pressure is", pressure)
+
+
+
+# show values on Sense HAT / one-time
+
 sense.show_message(temp)
 
 
